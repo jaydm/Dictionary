f265d28b272e91b02a76d6673f50aa54f6ff7bcb
Jay D. McHugh <jaydmchugh@gmail.com>
2019-09-24 13:53:10 -0500

    Completed functionality and added some debugging comments and logging

--- Dictionarys.cs
+++ Dictionarys.cs
@@ -1,112 +1,230 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace GenericImplementation
 {
-    class Dictionarys<K, V>
+  class Dictionarys<K, V>
+  {
+    // (jdm) this is not actually the table size since you
+    // allow for the dictionary to be created with a specific
+    // capacity - it is just the default
+    private static int DEFAULT_TABLE_SIZE = 127;
+    private Entry<K, V>[] table; // (jdm) hide direct access to the internal structure
+    private int count; // (jdm) hide direct access to the internals
+
+    public Dictionarys()
     {
-        static int TABLE_SIZE = 127;
-        Entry<K, V>[] table;
-        int count;
+      table = new Entry<K, V>[DEFAULT_TABLE_SIZE];
+    }
 
-        public Dictionarys()
-        {
-            table = new Entry<K, V>[TABLE_SIZE];
-        }
-        public Dictionarys(int capacity)
-        {
-            table = new Entry<K, V>[capacity];
-        }
-        public void Insert(K key, V value)
-        {
-            int hashf = Math.Abs(key.GetHashCode() % table.Length);
-            //int hashf = key.GetHashCode() & 0x7FFFFFFF;  // another good way; 0x7FFFFFF remove the - sign;
-            //hashf %= table.Length;
-            Entry<K, V> entry = new Entry<K, V>(key, value);
-            Entry<K, V> doesExist = table[hashf]; //marking
-            if (doesExist == null)
-            {
-                table[hashf] = new Entry<K, V>(key, value); // insert if null
-                count++;
-            }
-            else
-            {
-                // check if key already exist
-                while (doesExist != null)
-                {
-                    if (doesExist.Key.Equals(key))
-                    {
-                        doesExist.Value = value;
-                        return;
-                    }
-                    doesExist = doesExist.Next; // to the next guy
-                }
+    public Dictionarys(int capacity)
+    {
+      table = new Entry<K, V>[capacity];
+    }
 
-                count++;
+    // (jdm) convenience function
+    private int getSlot(K key)
+    {
+      return Math.Abs(key.GetHashCode() % table.Length);
+    }
 
-            }
-        }
-        public V Get(K key)
-        {
-            int hashf = Math.Abs(key.GetHashCode() % table.Length);
-            Entry<K, V> slot = table[hashf];
-            if (!key.Equals(slot.Key) ) // if the key doesnt exist here
-            {
-             throw new ArgumentException("Key doesnt exist");
-      
-            }
-            while (slot != null)
-            {
-                if (key.Equals(slot.Key))
-                {
-                    return slot.Value;
-                }
+    public void Insert(K key, V value)
+    {
+      Console.WriteLine("Inserting value: {1} in key: {0}", key, value);
 
-                slot = slot.Next;
-            }
-            return default(V);
-        }
-        public bool exist(K key)
-        {
-            return Get(key) != null;
-        }
-        public int Count { get { return this.count; } }
+      int hashf = getSlot(key);
 
-        public V this[K key]
+      Console.WriteLine("Slot number: {0}", hashf);
+
+      //int hashf = key.GetHashCode() & 0x7FFFFFFF;  // another good way; 0x7FFFFFF remove the - sign;
+      //hashf %= table.Length;
+
+      Entry<K, V> entry = new Entry<K, V>(key, value);
+
+      // (jdm) changing the name to be more descriptive
+      // Entry<K, V> doesExist = table[hashf]; //marking
+      Entry<K, V> slot = table[hashf];
+
+      if (slot == null) // (jdm) no entries in the slot yet...
+      {
+        Console.WriteLine("Empty slot...set table index to entry");
+
+        table[hashf] = entry;
+
+        count++;
+      }
+      else
+      {
+        // check if key already exist
+        Console.WriteLine("Non-empty slot...search for existing key...");
+
+        while (slot != null)
         {
-            get { return Get(key); }
-            set { Insert(key, value); }
+          if (slot.Key.Equals(key))
+          {
+            Console.WriteLine("Key found...overwrite value");
+
+            slot.Value = value;
+
+            return;
+          }
+
+          slot = slot.Next; // to the next guy
         }
-        public void Clear()
+
+        // (jdm) the key was not found...so, add it to the beginning of the
+        // list and fix the pointer
+        Console.WriteLine("Key not found...insert entry at beginning of collision list...");
+
+        Console.WriteLine("Original first entry in slot: key = {0} value = {1}", table[hashf].Key, table[hashf].Value);
+        Console.WriteLine("New value should be: {0}: ", entry.Value);
+
+        entry.Next = table[hashf];
+        table[hashf] = entry;
+
+        Console.WriteLine("New value in first entry of slot: {0}", table[hashf].Value);
+
+        Entry<K, V> secondEntry = table[hashf].Next;
+
+        Console.WriteLine("Second entry: key = {0} value = {1}", secondEntry.Key, secondEntry.Value);
+
+        count++;
+      }
+    }
+
+    public V Get(K key)
+    {
+      int hashf = getSlot(key); // (jdm) Use convenience function
+      Entry<K, V> pointer = table[hashf]; // (jdm) renaming for style only
+
+      // (jdm) cannot test this here - the matching key could be somewhere
+      // other than the beginning of the collision list
+      /*
+      if (!key.Equals(pointer.Key)) // if the key doesnt exist here
+      {
+        throw new ArgumentException("Key doesnt exist");
+
+      }
+      */
+
+      while (pointer != null)
+      {
+        if (key.Equals(pointer.Key))
         {
-            table = new Entry<K, V>[table.Length]; // all null
-            count = 0; // set count to zero
+          return pointer.Value;
         }
-        public void Remove(K key)
-        {
-            
-            int hashf = Math.Abs(key.GetHashCode() % table.Length);
-            Entry<K, V> slot = table[hashf];
-            Entry<K, V> prev = null;
+
+        pointer = pointer.Next;
+      }
+
+      // (jdm) shouldn't this also return key does not exist?
+      // return default(V);
+      throw new ArgumentException("Key doesnt exist");
+    }
+
+    public bool exist(K key)
+    {
+      return Get(key) != null;
+    }
+
+    public int Count { get { return this.count; } }
+
+    public V this[K key]
+    {
+      get { return Get(key); }
+      set { Insert(key, value); }
+    }
+
+    public void Clear()
+    {
+      table = new Entry<K, V>[table.Length]; // all null
+      count = 0; // set count to zero
+    }
+
+    // (jdm) here is where there should be some direction on
+    // the behaviour of the function when attempting to
+    // remove a non-existant key
+    // either: silently fail or throw an exception
+    //
+    // it looks like you want to throw an exception
+    // so there are some changes below
+    public void Remove(K key)
+    {
+      int hashf = getSlot(key);
+
+      Entry<K, V> slot = table[hashf];
+      Entry<K, V> prev = null;
+
+      // (jdm) there is nothing in the entire slot - so the key cannot be in the list
+      if (slot == null)
+      {
+        throw new ArgumentException("Cannot remove...key does not exist");
+      }
+
+      // (jdm) the original would report failure if the selected key
+      // was not the first entry in the collision list
+      /*
             if (!key.Equals(slot.Key) && slot == null) // if the key doesnt exist here
             {
-                throw new ArgumentException("Key doesnt exist");
+              throw new ArgumentException("Key doesnt exist");
             }
-            while (slot != null)
-            {
-                prev = slot;
-                slot = slot.Next;
-            }
+      */
 
-            
-                if(prev != null)
-                {                                 
-                    table[hashf] = null; // set required key to null
-                }
-          
-            count--;
-            return ;
+      // (jdm) the original code was stepping through all of the entries
+      // in the selected slot - but was not checking for a key match
+      // 
+      // then it remove the last entry in the list...there is no way
+      // of being certain that was actually a match for the key you are
+      // searching for. in fact, you could be removing the wrong key and
+      // leaving the one you want to remove
+      /*
+      while (slot != null)
+      {
+        prev = slot;
+        slot = slot.Next;
+      }
+
+      if (prev != null)
+      {
+        table[hashf] = null; // set required key to null
+      }
+      */
+
+      while (slot != null)
+      {
+        if (slot.Key.Equals(key))
+        {
+          if (prev != null)
+          {
+            prev.Next = slot.Next; // (jdm) change next pointer of the previous entry to the entry after current
+          }
+          else
+          {
+            // (jdm) prev == null...so, this is the first entry in the list
+            table[hashf] = slot.Next;
+          }
+
+          count--;
+
+          return;
         }
+
+        prev = slot;
+        slot = slot.Next;
+      }
+
+      // (jdm) since the expected behaviour is to throw an exception when
+      // trying to remove a non-existant key...we need to throw an exception
+      // because the key was not already removed
+      // also, we should not decrement the count
+      /*
+      count--;
+
+      return;
+      */
+      
+      throw new ArgumentException("Cannot remove...key does not exist");
     }
+  }
 }
--- Program.cs
+++ Program.cs
@@ -1,21 +1,69 @@
-﻿using System;
+using System;
 
 namespace GenericImplementation
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Dictionarys<int, int> dic = new Dictionarys<int, int>();
-            dic.Insert(2, 567);
-            dic.Insert(4, 897);
-            dic.Insert(7, 8898);
-            dic.Insert(6, 6665);
-            //  
-            // dic.Clear();
-            Console.WriteLine(   dic.Remove(2));
-            Console.WriteLine(dic[4]);
-            Console.WriteLine(dic.Count);
-        }
+      Dictionarys<int, int> dic = new Dictionarys<int, int>();
+
+      dic.Insert(2, 567);
+
+      Console.WriteLine("What is in dictionary for key = 2...{0}", dic.Get(2));
+
+      dic.Insert(4, 897);
+
+      Console.WriteLine("What is in dictionary for key = 4...{0}", dic.Get(4));
+
+      dic.Insert(4, 1897);
+
+      Console.WriteLine("What is in dictionary for key = 4...{0}", dic.Get(4));
+
+      // (jdm) forcing an instance where there is more than one
+      // value in the same slot
+      // (you can only know this if you know the implementation details)
+      dic.Insert(4 + 127, 1000);
+
+      Console.WriteLine("What is in dictionary for key = 131 (4 + 127)...{0}", dic.Get(4 + 127));
+      Console.WriteLine("Is the value for key = 4 still 1897?...{0}", dic.Get(4));
+
+      dic.Insert(7, 8898);
+
+      Console.WriteLine("What is in dictionary for key = 7...{0}", dic.Get(7));
+
+      dic.Insert(6, 6665);
+
+      Console.WriteLine("What is in dictionary for key = 6...{0}", dic.Get(6));
+
+      // dic.Clear();
+      // (jdm) can't console out the result of dic.Remove - it returns void
+      // Console.WriteLine(dic.Remove(2));
+
+      Console.WriteLine("Dictionary size: {0}", dic.Count);
+      Console.WriteLine("Does an entry for key = 2 exist? {0}", dic.exist(2));
+      Console.WriteLine("Removing the value for key = 2");
+
+      dic.Remove(2);
+
+      try
+      {
+        Console.WriteLine("How about now? {0}", dic.exist(2));
+      }
+      catch (ArgumentException e)
+      {
+        Console.WriteLine("There is no longer a value for key = 2: {0}", e.Message);
+      }
+
+      Console.WriteLine("Dictionary size after removal: {0}", dic.Count);
+
+      // (jdm) this is not quite what you need - you should use dic.Get(4)
+      // but...it seems to work. must be some kind of language overloading
+      Console.WriteLine("Dictionary[4]: {0}", dic[4]);
+
+      Console.WriteLine("Dictionary Get(4): {0}", dic.Get(4));
+      Console.WriteLine("Dictionary Get(4 + 127): {0}", dic.Get(4 + 127));
+      Console.WriteLine("Dictionary Count: {0}", dic.Count);
     }
+  }
 }
